d800e427382200a7578932ea547ae539ddfe20bc
nlukatska <nelli.lukatska@adquota.com>
2015-02-17 17:44:08 +0200

    refactor HW and create 3 modules

--- HW.rb
+++ HW.rb
@@ -1,102 +1,52 @@
 require 'watir-webdriver'
+require_relative 'ModuleRegisterLogin'
+require_relative 'ModuleCreateProject'
+require_relative 'ModuleCreateIssues'
+include RegisterAndLogin
+include CreateProject
+include CreateIssues
 
-browser = Watir::Browser.new :firefox
+@browser = Watir::Browser.new :firefox
 
+@browser.goto 'http://demo.redmine.org'
+username = 'nel51'
+password = 'qwerty'
 
-browser.goto 'http://demo.redmine.org'
-username = 'nel48'
+register_user(username,password)
+fail unless @browser.link(class:'user').text.include? username
 
-# Register
-browser.link(class: 'register').click
-browser.text_field(id:'user_login').set(username)
-browser.text_field(id:'user_password').set('qwerty')
-browser.text_field(id:'user_password_confirmation').set('qwerty')
-browser.text_field(id:'user_firstname').set('nel'+username)
-browser.text_field(id:'user_lastname').set('nel'+username)
-browser.text_field(id:'user_mail').set(username+'@ffff.com')
-browser.button(value:'Submit').click
+logout
+#TODO add assert
 
-fail unless browser.link(class:'user').text.include? username
+login(username,password)
+fail unless @browser.link(class:'user').text.include? username
 
-#Logout
-browser.link(class: 'logout').click
+change_password(password)
+fail unless @browser.div(id: 'flash_notice').text.include? 'Password was successfully updated.'
 
+create_project(username+'testproject')
+fail unless @browser.div(id:'flash_notice').text.include? 'Successful creation.'
 
-#Login
-browser.link(class: 'login').click
-browser.text_field(id: 'username').set(username)
-browser.text_field(id:'password').set('qwerty')
-browser.button(name: 'login').click
-
-fail unless browser.link(class:'user').text.include? username
-
-#ChangePassword
-browser.link(class: 'my-account').click
-browser.link(class:'icon icon-passwd').click
-browser.text_field(id: 'password').set('qwerty')
-browser.text_field(id: 'new_password').set('qwerty1')
-browser.text_field(id: 'new_password_confirmation').set('qwerty1')
-browser.button(name: 'commit').click
-
-fail unless browser.div(id: 'flash_notice').text.include? 'Password was successfully updated.'
-
-
-#CreateProject
-browser.link(class:'projects').click
-browser.link(class:'icon-add').click
-
-browser.text_field(id:'project_name').set username+'test project'
-browser.text_field(id:'project_identifier').set username+'1234567890'
-browser.button(value:'Create').click
-
-fail unless browser.div(id:'flash_notice').text.include? 'Successful creation.'
-
-#AddUsersToProject
-browser.link(id: 'tab-members').click
-browser.text_field(id: 'principal_search' ).set 'nel1'
-sleep 2
-browser.checkbox(xpath: "//*[@id='principals']/label[contains(text(),'nel1')]/input").set
-browser.checkbox(xpath: ".//*[@id='new_membership']/fieldset/p[2]/label[2]/input").set
-browser.button(id: 'member-add-submit').click
-
+add_user_to_project
 #fail unless browser.table(class:'list members').text.include? 'nel1'
 
-#EditProjectVersion
-browser.link(id: 'tab-versions').click
-browser.link(class: 'icon icon-add').click
-browser.text_field(id:'version_name').set 'version'+username
-browser.button(name: 'commit').click
-
-fail unless browser.div(id:'flash_notice').text.include? 'Successful creation.'
-
-#CreateABug
-browser.link(class: 'new-issue').click
-browser.text_field(id:'issue_subject').set 'test issue bug'
-browser.button(name: 'commit').click
-
-fail unless browser.div(id: 'flash_notice').text.include? 'created'
-fail unless browser.div(id: 'content').h2.text.include? 'Bug'
-
-#CreateAFeature
-browser.link(class: 'new-issue').click
-browser.select_list(id: 'issue_tracker_id').select('Feature')
-sleep 2
-browser.text_field(id:'issue_subject').set 'test issue feature'
-browser.button(name: 'commit').click
-
-fail unless browser.div(id: 'flash_notice').text.include? 'created'
-fail unless browser.div(id: "content").h2.text.include? 'Feature'
+edit_project_version('version'+username)
+fail unless @browser.div(id:'flash_notice').text.include? 'Successful creation.'
 
 
-#CreateASupport
-browser.link(class: 'new-issue').click
-browser.select_list(id: 'issue_tracker_id').select('Support')
-sleep 2
-browser.text_field(id:'issue_subject').set 'test issue support'
-browser.button(name: 'commit').click
+create_bug('test bug')
+fail unless @browser.div(id: 'flash_notice').text.include? 'created'
+fail unless @browser.div(id: 'content').h2.text.include? 'Bug'
 
-fail unless browser.div(id: 'flash_notice').text.include? 'created'
-fail unless browser.div(id: "content").h2.text.include? 'Support'
+
+create_feature('test feature')
+fail unless @browser.div(id: 'flash_notice').text.include? 'created'
+fail unless @browser.div(id: "content").h2.text.include? 'Feature'
+
+
+create_support('test support')
+fail unless @browser.div(id: 'flash_notice').text.include? 'created'
+fail unless @browser.div(id: "content").h2.text.include? 'Support'
 
 #VisibilityOfIssues
-browser.link(class: 'issues').click
+@browser.link(class: 'issues').click
---
+++ ModuleRegisterLogin.rb
@@ -0,0 +1,37 @@
+module RegisterAndLogin
+
+def register_user(username,password)
+  @browser.link(class: 'register').click
+  @browser.text_field(id: 'user_login').set(username)
+  @browser.text_field(id: 'user_password').set(password)
+  @browser.text_field(id: 'user_password_confirmation').set(password)
+  @browser.text_field(id: 'user_firstname').set('nel'+username)
+  @browser.text_field(id: 'user_lastname').set('nel'+username)
+  @browser.text_field(id: 'user_mail').set(username+'@test.com')
+  @browser.button(value: 'Submit').click
+end
+
+
+def logout
+  @browser.link(class: 'logout').click
+end
+
+
+def login(username, password)
+  @browser.link(class: 'login').click
+  @browser.text_field(id: 'username').set(username)
+  @browser.text_field(id: 'password').set(password)
+  @browser.button(name: 'login').click
+end
+
+def change_password(oldpassword)
+  @browser.link(class: 'my-account').click
+  @browser.link(class: 'icon icon-passwd').click
+  @browser.text_field(id: 'password').set(oldpassword)
+  @browser.text_field(id: 'new_password').set(oldpassword+'1')
+  @browser.text_field(id: 'new_password_confirmation').set(oldpassword+'1')
+  @browser.button(name: 'commit').click
+end
+
+end
+
